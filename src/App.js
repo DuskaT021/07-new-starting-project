7ebe82064aa0aa3e4232e8007383483e6cd4c169
DuskaT021 <aleksa2601@gmail.com>
2021-06-17 21:36:14 +0200

    Added Switch and Route component to App.js and BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
 function App() {
-  return <div>Hello</div>;
+  return <div>
+    <Switch>
+      <Route path='/' exact={true}>
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+    </Switch>
+  </div>;
 }
 
 export default App;

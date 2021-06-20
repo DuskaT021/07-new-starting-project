d0b0c97370504171fb7ec88fcfdfc1a463c208f4
DuskaT021 <aleksa2601@gmail.com>
2021-06-20 23:56:37 +0200

    Added the Layout.js wrapper component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout'
 
 function App() {
-  return <div>
-    <MainNavigation/ >
+  return(
+  <Layout>
+    
     {/* {https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25601666#:~:text=465.%20Outputting%20Lists%20of%20Data } */}
     <Switch>
       <Route path='/' exact={true}>
@@ -20,7 +21,8 @@ function App() {
         <FavoritesPage />
       </Route>
     </Switch>
-  </div>;
+  </Layout>
+  );
 }
 
 export default App;
